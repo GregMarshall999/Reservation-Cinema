823d6ad163f247dcdcb16403ad6912e41cc67ed0
Greg <gregory.marshall999@gmail.com>
2025-01-11 12:52:12 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReservationCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
